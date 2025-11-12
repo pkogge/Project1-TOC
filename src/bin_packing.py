7da1bac2bdeba860fac009a0bcc0970b029f52e9
Verite Mugabo <mugaboverite@gmail.com>
2025-11-12 12:54:00 -0500

    adds initial implementation

--- src/bin_packing.py
+++ src/bin_packing.py
@@ -28,6 +28,8 @@ instance_id,bin_capacity,bins_array,method,time_taken
 """
 
 from typing import List
+from itertools import product
+from collections import deque
 
 from src.helpers.bin_packing_helper import BinPackingAbstractClass
 
@@ -43,12 +45,38 @@ class BinPacking(BinPackingAbstractClass):
     def binpacking_backtracing(
         self, bin_capacity: int, clauses: List[int]
     ) -> List[List[int]]:
-        return []
+        res = []
+
+        def backtrack(start: int, path: list[int], total: int):
+            if total == bin_capacity:
+                res.append(path[:])
+                return
+
+            if total > bin_capacity:
+                return
+
+            for i in range(start, len(clauses)):
+                path.append(clauses[i])
+                backtrack(i, path, total + clauses[i])
+                path.pop()
+
+        backtrack(0, [], 0)
+
+        return res
 
     def binpacking_bruteforce(
         self, bin_capacity: int, clauses: List[int]
     ) -> List[List[int]]:
-        return []
+        res = []
+        clauses = sorted(clauses)
+        bounds = [(bin_capacity // c) for c in clauses]
+
+        for counts in product(*[range(b+1) for b in bounds]):
+            if sum(c*n for c, n in zip(clauses, counts)) == bin_capacity:
+                comb = [c for c, n in zip(clauses, counts) for _ in range(n)]
+                res.append(comb)
+
+        return res
 
     def binpacking_simple(
         self, bin_capacity: int, clauses: List[int]
@@ -58,4 +86,25 @@ class BinPacking(BinPackingAbstractClass):
     def binpacking_bestcase(
         self, bin_capacity: int, clauses: List[int]
     ) -> List[List[int]]:
-        return []
+        res = []
+        clauses = sorted(clauses)
+        q = deque()
+        q.append((0, 0, []))
+
+        while q:
+            curr_sum, i, curr_combination = q.popleft()
+            if  curr_sum == bin_capacity:
+                res.append(curr_combination)
+                continue
+
+            if i >= len(clauses):
+                continue
+
+            curr = clauses[i]
+            max_ntimes = (bin_capacity - curr_sum) // curr
+            for k in range(max_ntimes + 1):
+                new_sum = curr_sum + k * curr
+                if new_sum <= bin_capacity:
+                    q.append((new_sum, i + 1, curr_combination + [curr] * k))
+
+        return res
